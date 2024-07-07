f857e3d5b57083d448b60d4e53ba1d0fc62215b3
stella <lisyukstella0@gmail.com>
2024-07-07 13:38:44 +0300

    add form

--- my-app/src/components/Form/Form.js
+++
@@ -1,9 +0,0 @@
-const Form = ({ handleSubmit, children }) => (
-    <form onSubmit={handleSubmit}>
-      {children}
-      <input type="submit" />
-    </form>
-  );
-  
-  export default Form;
-  
--- my-app/src/containers/Products/Products.js
+++ my-app/src/containers/Products/Products.js
@@ -1,16 +1,20 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductsButton from '../../components/ProductsButton/ProductsButton';
 import { IoMdPerson, IoMdAdd } from 'react-icons/io';
 import formIcon from '../../assets/FormIcon.png';
 import './Products.css';
 import Table from '../../components/Table/Table';
+import Form from './components/Form/Form';
 import { API_URL } from '../../constants';
 import { useNavigate } from 'react-router-dom';
 
 const Products = () => {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [formTitle, setFormTitle] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -35,6 +39,18 @@ const Products = () => {
     navigate('/preview');
   };
 
+  const handleAddProductClick = () => {
+    setFormTitle('Add Product');
+    setSelectedProduct(null);
+    setShowForm(true);
+  };
+
+  const handleEditProductClick = (product) => {
+    setFormTitle('Edit Product');
+    setSelectedProduct(product);
+    setShowForm(true);
+  };
+
   const handleDeleteProduct = async (product) => {
     try {
       await axios.delete(`${API_URL}/api/v1/products/${product.id}`);
@@ -43,6 +59,12 @@ const Products = () => {
       console.error('Error deleting product:', error);
     }
   };
+
+  const handleFormCancel = () => {
+    setShowForm(false);
+    setSelectedProduct(null);
+  };
+
 
   return (
     <div className="products">
@@ -61,12 +83,20 @@ const Products = () => {
           label="Add Product"
           className="add-product-button"
           Icon={IoMdAdd}
+          onClick={handleAddProductClick}
         />
       </div>
       <h1>Products</h1>
       <div className='table-container'>
-        <Table products={products} onDelete={handleDeleteProduct} />
+        <Table products={products} onDelete={handleDeleteProduct} onEdit={handleEditProductClick} />
       </div>
+      {showForm && (
+        <Form
+          title={formTitle}
+          product={selectedProduct}
+          onCancel={handleFormCancel}
+        />
+      )}
     </div>
   );
 };
